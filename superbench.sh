609f4bdf9285fcbd7f93d50d6b93f21466ffdbe1
qd201211 <55730160+qd201211@users.noreply.github.com>
2020-03-27 21:17:05 +0800

    Update superbench.sh

--- superbench.sh
+++ superbench.sh
@@ -213,9 +213,9 @@ speed_test(){
 	        local REDownload=$(echo "$temp" | awk -F ':' '/Download/{print $2}')
 	        local reupload=$(echo "$temp" | awk -F ':' '/Upload/{print $2}')
 	        local relatency=$(echo "$temp" | awk -F ':' '/Hosted/{print $2}')
-	        #local relatency=$(pingtest $3)
-	        #temp=$(echo "$relatency" | awk -F '.' '{print $1}')
-        	#if [[ ${temp} -gt 1000 ]]; then
+	        local relatency=$(pingtest $3)
+	        temp=$(echo "$relatency" | awk -F '.' '{print $1}')
+        	if [[ ${temp} -gt 1000 ]]; then
             	relatency=" - "
         	#fi
 	        local nodeName=$2
