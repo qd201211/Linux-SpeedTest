8e7a1fb181631efe92455bf1ee3de2ce4d06d951
qd201211 <55730160+qd201211@users.noreply.github.com>
2020-09-05 16:33:18 +0800

    Update superbench.sh

--- superbench.sh
+++ superbench.sh
@@ -21,7 +21,7 @@ about() {
 	echo " ========================================================= "
 	echo " \                 Superbench.sh  测试脚本               / "
 	echo " \         系统基础信息, I/O 测试 以及 网络速度测试      / "
-	echo " \                   v1.1.7 (10 Apr 2020)                / "
+	echo " \                   v2.0.0 (6 Sep 2020)                / "
 	echo " \                   代码由 Oldking 编写                 / "
 	echo " \              修改以及汉化由 qd201211 完成             / "
 	echo " ========================================================= "
@@ -66,10 +66,7 @@ benchinit() {
 
 	# check python
 	if  [ ! -e '/usr/bin/python' ]; then
-	        #echo -e
-	        #read -p "${RED}Error:${PLAIN} python is not install. You must be install python command at first.\nDo you want to install? [y/n]" is_install
-	        #if [[ ${is_install} == "y" || ${is_install} == "Y" ]]; then
-	        echo " Installing Python ..."
+	        echo "正在安装 Python"
 	            if [ "${release}" == "centos" ]; then
 	            		yum update > /dev/null 2>&1
 	                    yum -y install python > /dev/null 2>&1
@@ -85,10 +82,7 @@ benchinit() {
 
 	# check curl
 	if  [ ! -e '/usr/bin/curl' ]; then
-	    #echo -e
-	    #read -p "${RED}Error:${PLAIN} curl is not install. You must be install curl command at first.\nDo you want to install? [y/n]" is_install
-	    #if [[ ${is_install} == "y" || ${is_install} == "Y" ]]; then
-	        echo " Installing Curl ..."
+	        echo "正在安装 Curl"
 	            if [ "${release}" == "centos" ]; then
 	                yum update > /dev/null 2>&1
 	                yum -y install curl > /dev/null 2>&1
@@ -96,17 +90,11 @@ benchinit() {
 	                apt-get update > /dev/null 2>&1
 	                apt-get -y install curl > /dev/null 2>&1
 	            fi
-	    #else
-	    #    exit
-	    #fi
 	fi
 
 	# check wget
 	if  [ ! -e '/usr/bin/wget' ]; then
-	    #echo -e
-	    #read -p "${RED}Error:${PLAIN} wget is not install. You must be install wget command at first.\nDo you want to install? [y/n]" is_install
-	    #if [[ ${is_install} == "y" || ${is_install} == "Y" ]]; then
-	        echo " Installing Wget ..."
+	        echo "正在安装 Wget"
 	            if [ "${release}" == "centos" ]; then
 	                yum update > /dev/null 2>&1
 	                yum -y install wget > /dev/null 2>&1
@@ -114,10 +102,14 @@ benchinit() {
 	                apt-get update > /dev/null 2>&1
 	                apt-get -y install wget > /dev/null 2>&1
 	            fi
-	    #else
-	    #    exit
-	    #fi
 	fi
+	
+	# check speedtest
+	if  [ ! -e './speedtest-cli/speedtest' ]; then
+		echo "正在安装 Speedtest-cli"
+		wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz > /dev/null 2>&1
+	fi
+	mkdir -p speedtest-cli && tar zxvf speedtest.tgz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
 
 	# install virt-what
 	#if  [ ! -e '/usr/sbin/virt-what' ]; then
@@ -142,29 +134,12 @@ benchinit() {
 	#	    apt-get -y install jq > /dev/null 2>&1
 	#	fi      
 	#fi
-
-	# install speedtest-cli
-	if  [ ! -e 'speedtest.py' ]; then
-		echo "正在安装 Speedtest-cli"
-		wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz > /dev/null 2>&1
-	fi
-	mkdir -p speedtest-cli && tar zxvf speedtest.tgz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
-
 	# install tools.py
 	if  [ ! -e 'tools.py' ]; then
 		echo " 正在安装 tools.py ..."
 		wget --no-check-certificate https://raw.githubusercontent.com/oooldking/script/master/tools.py > /dev/null 2>&1
 	fi
 	chmod a+rx tools.py
-
-	# install fast.com-cli
-	if  [ ! -e 'fast_com.py' ]; then
-		echo " 正在安装 Fast.com-cli ..."
-		wget --no-check-certificate https://raw.githubusercontent.com/sanderjo/fast.com/master/fast_com.py > /dev/null 2>&1
-		wget --no-check-certificate https://raw.githubusercontent.com/sanderjo/fast.com/master/fast_com_example_usage.py > /dev/null 2>&1
-	fi
-	chmod a+rx fast_com.py
-	chmod a+rx fast_com_example_usage.py
 
 	
 	sleep 5
@@ -184,103 +159,285 @@ next() {
 }
 
 speed_test(){
-	if [[ $1 == '' ]]; then
-		temp=$(python speedtest_original.py --share 2>&1)
-		is_down=$(echo "$temp" | grep 'Download')
-		result_speed=$(echo "$temp" | awk -F ' ' '/results/{print $3}')
-		if [[ ${is_down} ]]; then
-	        local REDownload=$(echo "$temp" | awk -F ':' '/Download/{print $2}')
-	        local reupload=$(echo "$temp" | awk -F ':' '/Upload/{print $2}')
-	        local relatency=$(echo "$temp" | awk -F ':' '/Hosted/{print $2}')
-
-	        temp=$(echo "$relatency" | awk -F '.' '{print $1}')
-        	if [[ ${temp} -gt 50 ]]; then
-            	relatency=" (*)"${relatency}
-        	fi
-	        local nodeName=$2
-
-	        temp=$(echo "${REDownload}" | awk -F ' ' '{print $1}')
+	speedLog="./speedtest.log"
+	true > $speedLog
+		speedtest-cli/speedtest -p no -s $1 --accept-license > $speedLog 2>&1
+		is_upload=$(cat $speedLog | grep 'Upload')
+		if [[ ${is_upload} ]]; then
+	        local REDownload=$(cat $speedLog | awk -F ' ' '/Download/{print $3}')
+	        local reupload=$(cat $speedLog | awk -F ' ' '/Upload/{print $3}')
+	        local relatency=$(cat $speedLog | awk -F ' ' '/Latency/{print $2}')
+	        
+			local nodeID=$1
+			local nodeLocation=$2
+			local nodeISP=$3
+			
+			strnodeLocation="${nodeLocation}　　　　　　"
+			LANG=C
+			#echo $LANG
+			
+			temp=$(echo "${REDownload}" | awk -F ' ' '{print $1}')
 	        if [[ $(awk -v num1=${temp} -v num2=0 'BEGIN{print(num1>num2)?"1":"0"}') -eq 1 ]]; then
-	        	printf "${YELLOW}%-17s${GREEN}%-18s${RED}%-20s${SKYBLUE}%-12s${PLAIN}\n" " ${nodeName}" "${reupload}" "${REDownload}" "${relatency}" | tee -a $log
-	        fi
-		else
-	        local cerror="ERROR"
-		fi
-	else
-		temp=$(python speedtest.py --server $1 --share 2>&1)
-		is_down=$(echo "$temp" | grep 'Download') 
-		if [[ ${is_down} ]]; then
-	        local REDownload=$(echo "$temp" | awk -F ':' '/Download/{print $2}')
-	        local reupload=$(echo "$temp" | awk -F ':' '/Upload/{print $2}')
-	        local relatency=$(echo "$temp" | awk -F ':' '/Hosted/{print $2}')
-	        #local relatency=$(pingtest $3)
-	        #temp=$(echo "$relatency" | awk -F '.' '{print $1}')
-        	#if [[ ${temp} -gt 1000 ]]; then
-            	relatency=" - "
-        	#fi
-	        local nodeName=$2
-
-	        temp=$(echo "${REDownload}" | awk -F ' ' '{print $1}')
-	        if [[ $(awk -v num1=${temp} -v num2=0 'BEGIN{print(num1>num2)?"1":"0"}') -eq 1 ]]; then
-	        	printf "${YELLOW}%-17s${GREEN}%-18s${RED}%-20s${SKYBLUE}%-12s${PLAIN}\n" " ${nodeName}" "${reupload}" "${REDownload}" "${relatency}" | tee -a $log
+	        	printf "${RED}%-6s${YELLOW}%s%s${GREEN}%-24s${CYAN}%s%-10s${BLUE}%s%-10s${PURPLE}%-8s${PLAIN}\n" "${nodeID}"  "${nodeISP}" "|" "${strnodeLocation:0:24}" "↑ " "${reupload}" "↓ " "${REDownload}" "${relatency}" | tee -a $log
 			fi
 		else
 	        local cerror="ERROR"
 		fi
-	fi
 }
 
-print_speedtest() {
-	printf "%-18s%-18s%-20s%-12s\n" " 节点名称" "     上传速度  " "        下载速度       "    "连接延迟 " | tee -a $log
-    speed_test '' '测速-官方  XG   '
-    speed_fast_com
-    speed_test '27377' '北京-电信  5G   '
-    speed_test '26352' '南京-电信  5G   '
-    speed_test '17145' '合肥-电信  5G   '
-	speed_test '27594' '广州-电信  5G   '
-	speed_test '26180' '济南-联通  5G   '
-	speed_test '27154' '天津-联通  5G   '
-	speed_test '24447' '上海-联通  5G   '
-	speed_test '26678' '广州-联通  5G   '
-	speed_test '27100' '青岛-移动  5G   '
-	speed_test '17184' '天津-移动  5G   '
-	speed_test '26850' '无锡-移动  5G   '
-	speed_test '27249' '南京-移动  5G   '
-	speed_test '26404' '合肥-移动  5G   '
-	speed_test '28491' '长沙-移动  5G   '
-	 
-	rm -rf speedtest.py
+selecttest() {
+	echo -e "  测速类型:    ${GREEN}1.${PLAIN} 三网测速    ${GREEN}2.${PLAIN} 取消测速"
+	echo -ne "               ${GREEN}3.${PLAIN} 电信节点    ${GREEN}4.${PLAIN} 联通节点    ${GREEN}5.${PLAIN} 移动节点"
+	while :; do echo
+			read -p "  请输入数字选择测速类型: " selection
+			if [[ ! $selection =~ ^[1-5]$ ]]; then
+					echo -ne "  ${RED}输入错误${PLAIN}, 请输入正确的数字!"
+			else
+					break   
+			fi
+	done
 }
 
-print_speedtest_fast() {
-	printf "%-18s%-18s%-20s%-12s\n" " 节点名称" "     上传速度  " "        下载速度       "    "连接延迟 " | tee -a $log
-    speed_test '' '测速-官方  XG   '
-    speed_fast_com
-    speed_test '27377' '北京-电信  5G   '
-	speed_test '26180' '济南-联通  5G   '
-	speed_test '27249' '南京-移动  5G   '
-	 
-	rm -rf speedtest.py
-}
+runtest() {
+	[[ ${selection} == 2 ]] && exit 1
 
-speed_fast_com() {
-	temp=$(python fast_com_example_usage.py 2>&1)
-	is_down=$(echo "$temp" | grep 'Result') 
-		if [[ ${is_down} ]]; then
-	        temp1=$(echo "$temp" | awk -F ':' '/Result/{print $2}')
-	        temp2=$(echo "$temp1" | awk -F ' ' '/Mbps/{print $1}')
-	        local REDownload="$temp2 Mbit/s"
-	        local reupload="0.00 Mbit/s"
-	        local relatency="-"
-	        local nodeName="奈飞-官方  XG  　"
+	if [[ ${selection} == 1 ]]; then
+		echo "----------------------------------------------------------------------"
+		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
+		start=$(date +%s) 
 
-	        printf "${YELLOW}%-18s${GREEN}%-18s${RED}%-20s${SKYBLUE}%-12s${PLAIN}\n" " ${nodeName}" "${reupload}" "${REDownload}" "${relatency}" | tee -a $log
+		 speed_test '3633' '上海' '电信'
+		 speed_test '24012' '内蒙古呼和浩特' '电信'
+		 speed_test '27377' '北京５Ｇ' '电信'
+		 speed_test '29026' '四川成都' '电信'
+		# speed_test '29071' '四川成都' '电信'
+		 speed_test '17145' '安徽合肥５Ｇ' '电信'
+		 speed_test '27594' '广东广州５Ｇ' '电信'
+		# speed_test '27810' '广西南宁' '电信'
+		 speed_test '27575' '新疆乌鲁木齐' '电信'
+		# speed_test '26352' '江苏南京５Ｇ' '电信'
+		 speed_test '5396' '江苏苏州５Ｇ' '电信'
+		# speed_test '5317' '江苏连云港５Ｇ' '电信'
+		# speed_test '7509' '浙江杭州' '电信'
+		 speed_test '23844' '湖北武汉' '电信'
+		 speed_test '29353' '湖北武汉５Ｇ' '电信'
+		 speed_test '28225' '湖南长沙５Ｇ' '电信'
+		 speed_test '3973' '甘肃兰州' '电信'
+		# speed_test '19076' '重庆' '电信'
+		#***
+		# speed_test '21005' '上海' '联通'
+		 speed_test '24447' '上海５Ｇ' '联通'
+		# speed_test '5103' '云南昆明' '联通'
+		 speed_test '5145' '北京' '联通'
+		# speed_test '5505' '北京' '联通'
+		# speed_test '9484' '吉林长春' '联通'
+		 speed_test '2461' '四川成都' '联通'
+		 speed_test '27154' '天津５Ｇ' '联通'
+		# speed_test '5509' '宁夏银川' '联通'
+		# speed_test '5724' '安徽合肥' '联通'
+		# speed_test '5039' '山东济南' '联通'
+		 speed_test '26180' '山东济南５Ｇ' '联通'
+		 speed_test '26678' '广东广州５Ｇ' '联通'
+		# speed_test '16192' '广东深圳' '联通'
+		# speed_test '6144' '新疆乌鲁木齐' '联通'
+		 speed_test '13704' '江苏南京' '联通'
+		 speed_test '5485' '湖北武汉' '联通'
+		# speed_test '26677' '湖南株洲' '联通'
+		 speed_test '4870' '湖南长沙' '联通'
+		# speed_test '4690' '甘肃兰州' '联通'
+		# speed_test '4884' '福建福州' '联通'
+		# speed_test '31985' '重庆' '联通'
+		 speed_test '4863' '陕西西安' '联通'
+		#***
+		# speed_test '30154' '上海' '移动'
+		# speed_test '25637' '上海５Ｇ' '移动'
+		# speed_test '26728' '云南昆明' '移动'
+		# speed_test '27019' '内蒙古呼和浩特' '移动'
+		 speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
+		# speed_test '30293' '内蒙古通辽５Ｇ' '移动'
+		 speed_test '25858' '北京' '移动'
+		 speed_test '16375' '吉林长春' '移动'
+		# speed_test '24337' '四川成都' '移动'
+		 speed_test '17184' '天津５Ｇ' '移动'
+		# speed_test '26940' '宁夏银川' '移动'
+		# speed_test '31815' '宁夏银川' '移动'
+		# speed_test '26404' '安徽合肥５Ｇ' '移动'
+		 speed_test '27151' '山东临沂５Ｇ' '移动'
+		# speed_test '25881' '山东济南５Ｇ' '移动'
+		# speed_test '27100' '山东青岛５Ｇ' '移动'
+		# speed_test '26501' '山西太原５Ｇ' '移动'
+		 speed_test '31520' '广东中山' '移动'
+		# speed_test '6611' '广东广州' '移动'
+		# speed_test '4515' '广东深圳' '移动'
+		# speed_test '15863' '广西南宁' '移动'
+		# speed_test '16858' '新疆乌鲁木齐' '移动'
+		 speed_test '26938' '新疆乌鲁木齐５Ｇ' '移动'
+		# speed_test '17227' '新疆和田' '移动'
+		# speed_test '17245' '新疆喀什' '移动'
+		# speed_test '17222' '新疆阿勒泰' '移动'
+		# speed_test '27249' '江苏南京５Ｇ' '移动'
+		# speed_test '21845' '江苏常州５Ｇ' '移动'
+		# speed_test '26850' '江苏无锡５Ｇ' '移动'
+		# speed_test '17320' '江苏镇江５Ｇ' '移动'
+		 speed_test '25883' '江西南昌５Ｇ' '移动'
+		# speed_test '17223' '河北石家庄' '移动'
+		# speed_test '26331' '河南郑州５Ｇ' '移动'
+		# speed_test '6715' '浙江宁波５Ｇ' '移动'
+		# speed_test '4647' '浙江杭州' '移动'
+		# speed_test '16503' '海南海口' '移动'
+		# speed_test '28491' '湖南长沙５Ｇ' '移动'
+		# speed_test '16145' '甘肃兰州' '移动'
+		 speed_test '16171' '福建福州' '移动'
+		# speed_test '18444' '西藏拉萨' '移动'
+		 speed_test '16398' '贵州贵阳' '移动'
+		 speed_test '25728' '辽宁大连' '移动'
+		# speed_test '16167' '辽宁沈阳' '移动'
+		# speed_test '17584' '重庆' '移动'
+		# speed_test '26380' '陕西西安' '移动'
+		# speed_test '29105' '陕西西安５Ｇ' '移动'
+		# speed_test '29083' '青海西宁５Ｇ' '移动'
+		# speed_test '26656' '黑龙江哈尔滨' '移动'
+
+		end=$(date +%s)  
+		rm -rf speedtest*
+		echo "----------------------------------------------------------------------"
+		time=$(( $end - $start ))
+		if [[ $time -gt 60 ]]; then
+			min=$(expr $time / 60)
+			sec=$(expr $time % 60)
+			echo -ne "  测试完成, 本次测速耗时: ${min} 分 ${sec} 秒"
 		else
-	        local cerror="ERROR"
+			echo -ne "  测试完成, 本次测速耗时: ${time} 秒"
 		fi
-	rm -rf fast_com_example_usage.py
-	rm -rf fast_com.py
+		echo -ne "\n  当前时间: "
+		echo $(date +%Y-%m-%d" "%H:%M:%S)
+		echo -e "  ${GREEN}# 三网测速中为避免节点数不均及测试过久，每部分未使用所${PLAIN}"
+		echo -e "  ${GREEN}# 有节点，如果需要使用全部节点，可分别选择三网节点检测${PLAIN}"
+	fi
 
+	if [[ ${selection} == 3 ]]; then
+		echo "----------------------------------------------------------------------"
+		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
+		start=$(date +%s) 
+
+		 speed_test '3633' '上海' '电信'
+		 speed_test '24012' '内蒙古呼和浩特' '电信'
+		 speed_test '27377' '北京５Ｇ' '电信'
+		 speed_test '29026' '四川成都' '电信'
+		 speed_test '29071' '四川成都' '电信'
+		 speed_test '17145' '安徽合肥５Ｇ' '电信'
+		 speed_test '27594' '广东广州５Ｇ' '电信'
+		 speed_test '27810' '广西南宁' '电信'
+		 speed_test '27575' '新疆乌鲁木齐' '电信'
+		 speed_test '26352' '江苏南京５Ｇ' '电信'
+		 speed_test '5396' '江苏苏州５Ｇ' '电信'
+		 speed_test '5317' '江苏连云港５Ｇ' '电信'
+		 speed_test '7509' '浙江杭州' '电信'
+		 speed_test '23844' '湖北武汉' '电信'
+		 speed_test '29353' '湖北武汉５Ｇ' '电信'
+		 speed_test '28225' '湖南长沙５Ｇ' '电信'
+		 speed_test '3973' '甘肃兰州' '电信'
+		 speed_test '19076' '重庆' '电信'
+
+		end=$(date +%s)  
+		rm -rf speedtest*
+		echo "----------------------------------------------------------------------"
+	fi
+	
+
+	if [[ ${selection} == 4 ]]; then
+		echo "----------------------------------------------------------------------"
+		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
+		start=$(date +%s) 
+
+		 speed_test '21005' '上海' '联通'
+		 speed_test '24447' '上海５Ｇ' '联通'
+		 speed_test '5103' '云南昆明' '联通'
+		 speed_test '5145' '北京' '联通'
+		 speed_test '5505' '北京' '联通'
+		 speed_test '9484' '吉林长春' '联通'
+		 speed_test '2461' '四川成都' '联通'
+		 speed_test '27154' '天津５Ｇ' '联通'
+		 speed_test '5509' '宁夏银川' '联通'
+		 speed_test '5724' '安徽合肥' '联通'
+		 speed_test '5039' '山东济南' '联通'
+		 speed_test '26180' '山东济南５Ｇ' '联通'
+		 speed_test '26678' '广东广州５Ｇ' '联通'
+		 speed_test '16192' '广东深圳' '联通'
+		 speed_test '6144' '新疆乌鲁木齐' '联通'
+		 speed_test '13704' '江苏南京' '联通'
+		 speed_test '5485' '湖北武汉' '联通'
+		 speed_test '26677' '湖南株洲' '联通'
+		 speed_test '4870' '湖南长沙' '联通'
+		 speed_test '4690' '甘肃兰州' '联通'
+		 speed_test '4884' '福建福州' '联通'
+		 speed_test '31985' '重庆' '联通'
+		 speed_test '4863' '陕西西安' '联通'
+
+		end=$(date +%s)  
+		rm -rf speedtest*
+		echo "----------------------------------------------------------------------"
+	fi
+
+
+	if [[ ${selection} == 5 ]]; then
+		echo "----------------------------------------------------------------------"
+		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
+		start=$(date +%s) 
+
+		 speed_test '30154' '上海' '移动'
+		 speed_test '25637' '上海５Ｇ' '移动'
+		 speed_test '26728' '云南昆明' '移动'
+		 speed_test '27019' '内蒙古呼和浩特' '移动'
+		 speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
+		 speed_test '30293' '内蒙古通辽５Ｇ' '移动'
+		 speed_test '25858' '北京' '移动'
+		 speed_test '16375' '吉林长春' '移动'
+		 speed_test '24337' '四川成都' '移动'
+		 speed_test '17184' '天津５Ｇ' '移动'
+		 speed_test '26940' '宁夏银川' '移动'
+		 speed_test '31815' '宁夏银川' '移动'
+		 speed_test '26404' '安徽合肥５Ｇ' '移动'
+		 speed_test '27151' '山东临沂５Ｇ' '移动'
+		 speed_test '25881' '山东济南５Ｇ' '移动'
+		 speed_test '27100' '山东青岛５Ｇ' '移动'
+		 speed_test '26501' '山西太原５Ｇ' '移动'
+		 speed_test '31520' '广东中山' '移动'
+		 speed_test '6611' '广东广州' '移动'
+		 speed_test '4515' '广东深圳' '移动'
+		 speed_test '15863' '广西南宁' '移动'
+		 speed_test '16858' '新疆乌鲁木齐' '移动'
+		 speed_test '26938' '新疆乌鲁木齐５Ｇ' '移动'
+		 speed_test '17227' '新疆和田' '移动'
+		 speed_test '17245' '新疆喀什' '移动'
+		 speed_test '17222' '新疆阿勒泰' '移动'
+		 speed_test '27249' '江苏南京５Ｇ' '移动'
+		 speed_test '21845' '江苏常州５Ｇ' '移动'
+		 speed_test '26850' '江苏无锡５Ｇ' '移动'
+		 speed_test '17320' '江苏镇江５Ｇ' '移动'
+		 speed_test '25883' '江西南昌５Ｇ' '移动'
+		 speed_test '17223' '河北石家庄' '移动'
+		 speed_test '26331' '河南郑州５Ｇ' '移动'
+		 speed_test '6715' '浙江宁波５Ｇ' '移动'
+		 speed_test '4647' '浙江杭州' '移动'
+		 speed_test '16503' '海南海口' '移动'
+		 speed_test '28491' '湖南长沙５Ｇ' '移动'
+		 speed_test '16145' '甘肃兰州' '移动'
+		 speed_test '16171' '福建福州' '移动'
+		 speed_test '18444' '西藏拉萨' '移动'
+		 speed_test '16398' '贵州贵阳' '移动'
+		 speed_test '25728' '辽宁大连' '移动'
+		 speed_test '16167' '辽宁沈阳' '移动'
+		 speed_test '17584' '重庆' '移动'
+		 speed_test '26380' '陕西西安' '移动'
+		 speed_test '29105' '陕西西安５Ｇ' '移动'
+		 speed_test '29083' '青海西宁５Ｇ' '移动'
+		 speed_test '26656' '黑龙江哈尔滨' '移动'
+
+		end=$(date +%s)  
+		rm -rf speedtest*
+		echo "----------------------------------------------------------------------"
+	fi
 }
 
 io_test() {
@@ -603,12 +760,6 @@ get_system_info() {
 	virt_check
 }
 
-print_intro() {
-	printf ' Superbench.sh -- https://www.oldking.net/350.html\n' | tee -a $log
-	printf " 模式  : \e${GREEN}%s\e${PLAIN}    版本 : \e${GREEN}%s${PLAIN}\n" $mode_name 1.1.6 | tee -a $log
-	printf ' 修复版本使用 : wget -qO- msoayu56.com/files/speedtest/superbench.sh | bash\n' | tee -a $log
-}
-
 sharetest() {
 	echo " 分享测试结果:" | tee -a $log
 	echo " 路 $result_speed" | tee -a $log
@@ -680,35 +831,17 @@ bench_all(){
 	next;
 	print_io;
 	next;
-	print_speedtest;
+	speed_test;
+	selecttest;
+	runtest;
+	rm -rf speedtest*
 	next;
 	print_end_time;
 	next;
 	cleanup;
+	rm -rf speedtest*
 	sharetest ubuntu;
 }
-
-fast_bench(){
-	mode_name="Fast"
-	about;
-	benchinit;
-	clear
-	next;
-	print_intro;
-	next;
-	get_system_info;
-	print_system_info;
-	ip_info4;
-	next;
-	print_io fast;
-	next;
-	print_speedtest_fast;
-	next;
-	print_end_time;
-	next;
-	cleanup;
-}
-
 
 
 
