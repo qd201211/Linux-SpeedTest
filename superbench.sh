11e2059745268daf4f32d7e65dde22f55b5c4750
qd201211 <55730160+qd201211@users.noreply.github.com>
2021-08-27 11:31:52 +0800

    Update superbench.sh
    修复无法测速

--- superbench.sh
+++ superbench.sh
@@ -116,7 +116,11 @@ benchinit() {
 	# check speedtest
 	if  [ ! -e './speedtest-cli/speedtest' ]; then
 		echo "正在安装 Speedtest-cli"
-		wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz > /dev/null 2>&1
+	#	mkdir -p /root/speedtest-cli && wget --no-check-certificate -O /root/speedtest-cli/speedtest https://raw.githubusercontent.com/user1121114685/speedtest_cli/master/spd_cli/x86_64/speedtest  > /dev/null 2>&1
+	#fi
+	#	chmod a+rx ./speedtest-cli/speedtest
+		wget --no-check-certificate -qO speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-$(uname -m)-linux.tgz
+		#wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz 
 	fi
 	mkdir -p speedtest-cli && tar zxvf speedtest.tgz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
 
